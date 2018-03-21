ab9a912c99ce8415f131e81cb6449f3f6e45a853
Brandon Vergara <brandonvergara@iMac-de-Brandon.local>
2018-03-21 09:50:25 -0600

    Adding yarn packages

--- MainVerticle.groovy
+++ MainVerticle.groovy
@@ -1,13 +1,43 @@
 package com.makingdevs.ticket
+//@Grapes(
+//    [
+//    @Grab(group='io.vertx', module='vertx-core', version='3.5.1'),
+//    @Grab(group='io.vertx', module='vertx-web', version='3.5.1'),
+//    @Grab(group='io.vertx', module='vertx-ext', version='30', type='pom'),
+//    @Grab(group='io.vertx', module='vertx-shell', version='3.5.1'),
+//    @Grab(group='io.vertx', module='vertx-dropwizard-metrics', version='3.0.0-milestone6')
+//    ]
+//)
+import io.vertx.ext.web.handler.StaticHandler
+import io.vertx.ext.web.Router
+import io.vertx.core.Vertx
+import io.vertx.ext.web.handler.sockjs.SockJSHandler
+import io.vertx.ext.dropwizard.MetricsService
 
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.DeploymentOptions
+println "Webserver ok"
 
-class MainVerticle extends AbstractVerticle {
-  @Override
-  void start(){
-    def config = vertx.currentContext().config()
-    DeploymentOptions options = new DeploymentOptions().setConfig(config)
-    vertx.deployVerticle("Webserver.groovy", options)
-  }
+//Vertx vertx = Vertx.vertx()
+def service = MetricsService.create(vertx)
+def server = vertx.createHttpServer()
+def router = Router.router(vertx)
+def sharedMap = vertx.sharedData().getLocalMap("cards")
+def opts = [
+            outboundPermitteds:[
+              [ address:"com.makingdevs.web.monitor" ]
+              ]
+           ]
+
+def ebHandler = SockJSHandler.create(vertx).bridge(opts)
+
+router.route("/eventbus/*").handler(ebHandler)
+router.route("/static/*").handler(StaticHandler.create().setCachingEnabled(false))
+
+Integer counter = 0
+
+vertx.setPeriodic(1000){ t ->
+  println "Enviando a web monitor"
+  vertx.eventBus().send("com.makingdevs.web.monitor", counter)
+  counter++
 }
+
+server.requestHandler(router.&accept).listen(8080)
---
+++ webroot/js/DirectorController.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var DirectorController = (function(){
+     var selectors = {
+         datepicker:'#datepicker',
+         datepicker1:'#datepicker1'
+    },
+    bindEvents = function bindEvents(){
+         console.log("Satrt bindEvents");
+         $(selectors.datepicker).datepicker({
+              setDate: new Date(),
+              dateFormat: 'dd-mm-yy'
+         });
+         $( "#datepickerQuotation" ).datepicker({
+             setDate: new Date(),
+             dateFormat: 'dd/mm/yy'
+        });
+         $(selectors.datepicker1).datepicker({
+              dateFormat: 'dd-mm-yy'
+        });
+         $(selectors.datepicker).datepicker('setDate', new Date());
+    };
+
+    var start = function(){
+    console.log("Start controller js");
+    bindEvents();
+
+    };
+    return{
+         start:start
+    }
+
+}());
+jQuery(function($){
+  QuotationController.start();
+});
