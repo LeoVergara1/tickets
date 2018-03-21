0cc596fb3ea6cac7f526fa5fe39d1d35db3b3a9c
Brandon Vergara <brandonvergara@iMac-de-Brandon.local>
2018-03-21 08:47:11 -0600

    Re-structure files

--- MainVerticle.groovy
+++
@@ -1,9 +0,0 @@
-import io.vertx.core.AbstractVerticle
-import io.vertx.core.eventbus.EventBus
-
-class MainVerticle {
-
-  MainVerticle(){
-    println "Init Verticles"
-  }
-}
---
+++ src/main/groovy/MainVerticle.groovy
@@ -0,0 +1,13 @@
+package com.makingdevs
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.DeploymentOptions
+
+class MainVerticle extends AbstractVerticle {
+  @Override
+  void start(){
+    def config = vertx.currentContext().config()
+    DeploymentOptions options = new DeploymentOptions().setConfig(config)
+    vertx.deployVerticle("src/main/groovy/com/makingdevs/Webserver.groovy", options)
+  }
+}
