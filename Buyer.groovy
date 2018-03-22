fc655b5174d387d5d98d833994bc37e7a9cee015
Brandon Vergara <brandon@makingdevs.com>
2018-03-22 10:43:44 -0600

    Changed message for map

--- Buyer.groovy
+++ Buyer.groovy
@@ -10,22 +10,23 @@ class Buyer extends AbstractVerticle {
   @Override
   void start() throws Exception {
     super.start()
-    def processId = Transform.getProcessId()
+    def processId = deploymentID()
     println "Proceso del cliente: ${processId}"
     def sd = vertx.sharedData
     EventBus eb = vertx.eventBus();
     println "Init Buyer"
     eb.consumer("com.ticket.init.vew.${processId}"){ message ->
-      println "Inicia proceso de vista con el boleto: ${message.body()}"
-      Ticket ticket = new Ticket(status:"view", place:message.body(), client: processId)
+      println "Inicia proceso de vista con el boleto: ${message.body().ticket}"
+      Ticket ticket = new Ticket(status:"view", place:message.body().ticket, client: processId)
       def jsonTicket = Transform.getJsonObjectFromClass(ticket)
       eb.send("com.ticket.office", jsonTicket){ reply ->
         println reply.result().body()
+        eb.send("com.makingdevs.comunicate.info.${message.body().processId}", reply.result().body())
       }
     }
 
     eb.consumer("com.ticket.status.${processId}"){ message ->
-      Ticket ticket = new Ticket(status:"bought", place:message.body(), client: processId)
+      Ticket ticket = new Ticket(status:"bought", place:message.body().ticket, client: processId)
       def jsonTicket = Transform.getJsonObjectFromClass(ticket)
       println message.body()
       eb.send("com.ticket.office.buy", jsonTicket){ reply ->
