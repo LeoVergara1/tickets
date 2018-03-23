e6e7e9a389455214a3d632749db6c89fc2ae7f0d
Brandon Vergara <brandon@makingdevs.com>
2018-03-23 10:04:18 -0600

    Adding timer seesion deploy

--- Buyer.groovy
+++ Buyer.groovy
@@ -4,6 +4,7 @@ import io.vertx.core.eventbus.EventBus
 import Ticket
 import Transform
 import io.vertx.core.json.JsonArray
+import io.vertx.core.DeploymentOptions
 
 class Buyer extends AbstractVerticle {
 
@@ -13,7 +14,7 @@ class Buyer extends AbstractVerticle {
     def processId = deploymentID()
     println "Proceso del cliente: ${processId}"
     def sd = vertx.sharedData
-    EventBus eb = vertx.eventBus();
+    EventBus eb = vertx.eventBus()
     println "Init Buyer"
     eb.consumer("com.ticket.init.vew.${processId}"){ message ->
       println "Inicia proceso de vista con el boleto: ${message.body().ticket}"
@@ -21,6 +22,11 @@ class Buyer extends AbstractVerticle {
       def jsonTicket = Transform.getJsonObjectFromClass(ticket)
       eb.send("com.ticket.office", jsonTicket){ reply ->
         println reply.result().body()
+        def mapFromFont = Transform.mapFromBodyJson(message.body())
+        mapFromFont.put("client", processId)
+        mapFromFont.put("place", message.body().ticket)
+        String deployIdTime = deploytmentSessionTime(mapFromFont)
+        println "Map desde el front: ${mapFromFont}"
         eb.publish("com.makingdevs.comunicate.info.${message.body().ticket}", [status: reply.result().body().message, count:reply.result().body().count])
       }
     }
@@ -40,5 +46,24 @@ class Buyer extends AbstractVerticle {
   void stop() throws Exception {
     def sd = vertx.sharedData
     def result = sd.getLocalMap("result")
+  }
+
+  private def deploytmentSessionTime(def jsonMap){
+    DeploymentOptions opts_1 = new DeploymentOptions()
+    opts_1.setWorker(true)
+    def sd = vertx.sharedData
+    EventBus eb = vertx.eventBus()
+    String deployIdTime
+    vertx.deployVerticle("SessionTimeVerticle.groovy", opts_1){ resultVerticle ->
+      if (resultVerticle.succeeded()){
+        deployIdTime = resultVerticle.result()
+        println "Id timer: ${deployIdTime}"
+        eb.send("com.ticket.session.time.${deployIdTime}", jsonMap)
+      }
+      else {
+        resultVerticle.cause().printStackTrace()
+      }
+    }
+    deployIdTime
   }
 }
